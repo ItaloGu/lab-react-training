65ba8e16a5cca2b65b706ccd91795f867e6d7428
Italo Gusmão <itamarques19@gmail.com>
2021-11-10 12:30:43 -0300

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import IdCard from './components/IdCard';
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -13,21 +12,8 @@ import DriverCard from './components/DriverCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
 
       <h1>Id Card</h1>
       <IdCard
